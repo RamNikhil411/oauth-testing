15d60c36f7e1e8f98091d77bb1ef331edfe2f313
RamNikhil411 <nikhil.b@orotron.com>
2025-06-30 08:58:57 +0530

    Updated signin page

--- src/routes/signin.tsx
+++ src/routes/signin.tsx
@@ -42,7 +42,7 @@ function SignIn() {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className=" flex items-center justify-center bg-gray-100">
       <div className="bg-white shadow-md rounded-2xl px-10 py-12 w-full max-w-md text-center">
         <h1 className="text-3xl font-bold text-gray-800 mb-6">Welcome to Esigns</h1>
 
